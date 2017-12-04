59db8dff04c7c777eab7226fcf87afdcd3b5ea62
Al Imran <imranmmcbd@users.noreply.github.com>
2017-12-04 21:09:03 +0600

    Cowboy_radio

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+var audio = new Audio('http://listen.shoutcast.com/cowboyradio');
+audio.volume = 0.1;
+audio.play();
+
+$('.trigger').click(function() {
+  if (audio.paused == false) {
+      audio.pause();
+      $('.fa-play').show();
+      $('.fa-pause').hide();
+      $('.music-card').removeClass('playing');
+  } else {
+      audio.play();
+      $('.fa-pause').show();
+      $('.fa-play').hide();
+      $('.music-card').addClass('playing');
+  }
+});
